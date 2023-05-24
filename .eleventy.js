57d4c999d3a6d2973ad411168aa95de4bff30b75
Tom Lauerman <tom@lauerman.dev>
2023-05-23 23:38:51 -0700

    Replace Gulp.js with 11ty config
    Remove image processing altogether, as it's just a big headache. Not to
    mention it's not super necessary for a small site like this anyway.

--- .eleventy.js
+++ .eleventy.js
@@ -1,36 +1,85 @@
+const path = require("node:path");
 const htmlmin = require("html-minifier");
 const yaml = require("js-yaml");
+// css
+const sass = require("sass");
+const postcss = require("postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+// img
+const Image = require("@11ty/eleventy-img");
 
-const htmlminConfig = {
-  useShortDoctype: true,
-  removeComments: true,
-  collapseWhitespace: true,
-};
+module.exports = function (eleventyConfig) {
+  // copy files
+  eleventyConfig.addPassthroughCopy({
+    // fonts
+    "node_modules/@fontsource/quicksand/files": "css/files",
+    // javascript
+    "node_modules/vanilla-tilt/dist/vanilla-tilt.min.js": "js/vanilla-tilt.min.js",
+    "src/js": "js",
+    // data
+    "src/dat": "dat",
+    // images
+    "src/img": "img",
+  });
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if (outputPath.endsWith(".html")) {
-      const minified = htmlmin.minify(content, htmlminConfig);
+  // html
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+      const minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+
       return minified;
-    } else {
-      return content;
     }
-  })
+
+    return content;
+  });
+
+  // yaml
   eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 
-  // gulp-managed dependencies
-  eleventyConfig.addWatchTarget("./src/_scss/");
-  eleventyConfig.addWatchTarget("./src/js/");
-  eleventyConfig.addWatchTarget("./src/img/");
-  eleventyConfig.addWatchTarget("./src/dat/");
+  // scss
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+      let sassResult = await sass.compileStringAsync(
+        inputContent, {
+        loadPaths: [
+          parsed.dir || ".",
+          this.config.dir.includes,
+          "node_modules",
+        ]
+      }
+      );
 
-  // give gulp 500 millis to build before refresh
-  eleventyConfig.setWatchThrottleWaitTime(500);
+      return async () => {
+        return sassResult.css;
+      };
+    }
+  });
+  eleventyConfig.addTransform("postcss", async function (content) {
+    if (this.page.outputPath && this.page.outputPath.endsWith(".css")) {
+      let postCssResult = await postcss([
+        autoprefixer,
+        cssnano,
+      ]).process(content, { from: this.page.outputPath });
+
+      return postCssResult.css
+    }
+
+    return content;
+  });
 
   return {
+    htmlTempalteEngine: "njk",
     dir: {
       input: "src",
       output: "dist",
-    }
+    },
   }
 }
